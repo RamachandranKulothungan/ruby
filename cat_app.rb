a98a139ed18e8c5d61198c8aa87742e0160f85f4
RamachandranKulothungan <ramachandran.kulo@gmail.com>
2020-09-10 14:49:14 +0530

    cat_app modified

--- cat_app.rb
+++ cat_app.rb
@@ -3,10 +3,14 @@ class Object
     #overriding class method const_missing method
     def self.const_missing(c)
         puts "#{c} is missing. Loading dynamically"
-        require_relative "./cat.rb"
-        Cat
+        require_relative "./#{c.downcase}"
+        #Cat
+        klass = const_get(c)
+        return klass if klass
+        raise "Class not found: #{c}"
     end
 
 end
 puffy_cat = Cat.new
 puffy_cat.talk
+
